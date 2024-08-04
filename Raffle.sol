5e4f72d48222642c0e9c432a6a7867f833cb6c98
Dhiman NAYAK <spidy@Sobuj>
2024-08-04 20:04:35 +0530

    chore: forge init

---
+++ Raffle.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+/**
+ * @title A sample Raffle contract
+ * @author Dhiman Nayak 
+ * @notice This contract is creating sample raffle
+ * @dev Implement Chainlink erfv2.5
+ */
+contract Raffle{
+
+    
+
+}
